7671496d08a71148593dc4b6a401a2262e1cd7e6
Randy Costner <costner.randy@gmail.com>
2023-04-22 09:13:56 -0400

    RSI Cleanup
    Renamed RSI Variable to RelativeStrength to add Clarity

--- Core/Calculations/RealitiveStrengthIndex.cs
+++ Core/Calculations/RealitiveStrengthIndex.cs
@@ -8,7 +8,7 @@ namespace StockTracker.Core.Calculations
 {
     public class RealitiveStrengthIndex
     {
-        protected readonly IList<RSI> dataList;
+        protected readonly IList<RelativeStrength> dataList;
 
         /// <summary>
         /// Creates an instatnce of the RSI caluclation library
@@ -20,7 +20,7 @@ namespace StockTracker.Core.Calculations
         /// </param>
         public RealitiveStrengthIndex(IList rsiData)
         {
-            dataList = (IList<RSI>) rsiData;
+            dataList = (IList<RelativeStrength>) rsiData;
         }
 
         /// <summary>
@@ -47,7 +47,7 @@ namespace StockTracker.Core.Calculations
 
             for (int i = 1; i < itemCount - 1; i++)
             {
-                RSI rSI = dataList[i];
+                RelativeStrength rSI = dataList[i];
 
                 //If both are zero the entry has never been set or trading was
                 //flat
@@ -78,7 +78,7 @@ namespace StockTracker.Core.Calculations
 
             for (int i = 0; i < itemCount - 1; i++)
             {
-                RSI rSI = dataList[i];
+                RelativeStrength rSI = dataList[i];
 
                 //never been calculated before
                 if(i == 0 && rSI.AvgGain == 0 && rSI.AvgLoss == 0)
@@ -121,7 +121,7 @@ namespace StockTracker.Core.Calculations
             List<ITradingStructure> tradingStructures = new(14);
             short counter = 0;
 
-            foreach(RSI rSI in dataList)
+            foreach(RelativeStrength rSI in dataList)
             {
                 if (counter == 14) break;
 
