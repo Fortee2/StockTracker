2aeccdf2694de7bc7d80a29ad755f713e02bef5a
Randy Costner <costner.randy@gmail.com>
2023-06-10 11:08:22 -0400

    Update TrueRange.cs
    Refactor

--- Core/Calculations/TrueRange.cs
+++ Core/Calculations/TrueRange.cs
@@ -2,33 +2,35 @@ using System;
 using System.Collections.Generic;
 using StockTracker.Core.Domain;
 using StockTracker.Core.Interfaces.Calculations;
+namespace StockTracker.Core.Calculations
+{
+    public class TrueRange:ICalculate<TrueRangeResponse> {
+        private List<Quote> _quotes;
 
-public class TrueRange:ICalculate<TrueRangeResponse> {
-    private List<Quote> _quotes;
+        public TrueRange(List<Quote> quotes){
+            _quotes = quotes;
+        }
+        
+        public List<TrueRangeResponse> Calculate(){
+            List<TrueRangeResponse> trueRangeResponses = new List<TrueRangeResponse>();
+        decimal HighMinusLow = 0,
+            HighMinusPreviousClose = 0,  
+            LowMinusPreviousClose = 0,
+            TrueRange = 0;
 
-    public TrueRange(List<Quote> quotes){
-        _quotes = quotes;
-    }
-    
-    public List<TrueRangeResponse> Calculate(){
-        List<TrueRangeResponse> trueRangeResponses = new List<TrueRangeResponse>();
-       decimal HighMinusLow = 0,
-        HighMinusPreviousClose = 0,  
-        LowMinusPreviousClose = 0,
-        TrueRange = 0;
+            foreach(var quote in _quotes){
+                HighMinusLow = quote.High - quote.Low;
+                HighMinusPreviousClose = Math.Abs( quote.High - quote.Close);
+                LowMinusPreviousClose = Math.Abs(quote.Low - quote.Close);
 
-        foreach(var quote in _quotes){
-            HighMinusLow = quote.High - quote.Low;
-            HighMinusPreviousClose = Math.Abs( quote.High - quote.Close);
-            LowMinusPreviousClose = Math.Abs(quote.Low - quote.Close);
+                TrueRange = HighMinusLow > HighMinusPreviousClose ? HighMinusLow : HighMinusPreviousClose;
+                TrueRange = TrueRange > LowMinusPreviousClose ? TrueRange : LowMinusPreviousClose;
 
-            TrueRange = HighMinusLow > HighMinusPreviousClose ? HighMinusLow : HighMinusPreviousClose;
-            TrueRange = TrueRange > LowMinusPreviousClose ? TrueRange : LowMinusPreviousClose;
+                trueRangeResponses.Add(new TrueRangeResponse(quote.ActivityDate, TrueRange));
+            }
 
-            trueRangeResponses.Add(new TrueRangeResponse(quote.ActivityDate, TrueRange));
+            return trueRangeResponses;
         }
 
-        return trueRangeResponses;
     }
-
 }
